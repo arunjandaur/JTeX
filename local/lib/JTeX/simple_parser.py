48b6a646496b428fbc87beb8c8fd7c3290422f3e
Arun Jandaur <arun.jandaur@gmail.com>
2014-09-22 10:41:56 -0700

    Fixed all indentation

--- local/lib/JTeX/simple_parser.py
+++ local/lib/JTeX/simple_parser.py
@@ -2,28 +2,27 @@ import re
 
 #This class can parse both JSON code as well as Python lists and dicts into LaTeX
 class simple_parser():
-	#Fetches the file at filepath and parses it to LaTeX
-	def parse_file_to_latex(self, filepath):
+    #Fetches the file at filepath and parses it to LaTeX
+    def parse_file_to_latex(self, filepath):
+        header = '\documentclass[12pt, titlepage]{article}\n\\begin{document}\n\n\\begin{itemize}\n' #Beginning of the document
+        body = '' #This is where the parsed code goes
+        end = '\end{itemize}\n\end{document}\n' #End of the document
 
-		header = '\documentclass[12pt, titlepage]{article}\n\\begin{document}\n\n\\begin{itemize}\n' #Beginning of the document
-		body = '' #This is where the parsed code goes
-		end = '\end{itemize}\n\end{document}\n' #End of the document
+        input_file = open(filepath, 'r') #Open the .json or .py file
 
-		input_file = open(filepath, 'r') #Open the .json or .py file
+        for line in input_file: #Loops through each line of the file
+            line = line.replace('\t', '').replace('\n', '') #Remove all the tabs and new lines
+            if ('[' in line) or ('{' in line): #An open delimiter indicates the start of a bulleted list
+                body += '\item ${Item:}$\n' + re.sub(r'[{[]', r'\\begin{itemize}', line) + '\n' #This line substitutes {'s and ['s with \begin{itemize}, which is the beginning of a bulleted LaTeX list. Also, notice that there is a \item called "Item". This is because one can't have two \begin{itemize} in a row. To avoid this, I insert the label "Item" to indicate the start of an inner bulleted list, without causing a LaTeX syntax error.
+            elif (']' in line) or ('}' in line): #A closed delimiter indicates the end of a bulleted list. Hence, \end{itemize}
+                body += re.sub(r'[}\]]', r'\\end{itemize}', line) + '\n'
+            else:
+                body += '\item $' + line + '$\n' #This is the typical case, where an item is surrounded by the LaTeX syntax for making a bullet point.
 
-		for line in input_file: #Loops through each line of the file
-			line = line.replace('\t', '').replace('\n', '') #Remove all the tabs and new lines
-			if ('[' in line) or ('{' in line): #An open delimiter indicates the start of a bulleted list
-				body += '\item ${Item:}$\n' + re.sub(r'[{[]', r'\\begin{itemize}', line) + '\n' #This line substitutes {'s and ['s with \begin{itemize}, which is the beginning of a bulleted LaTeX list. Also, notice that there is a \item called "Item". This is because one can't have two \begin{itemize} in a row. To avoid this, I insert the label "Item" to indicate the start of an inner bulleted list, without causing a LaTeX syntax error.
-			elif (']' in line) or ('}' in line): #A closed delimiter indicates the end of a bulleted list. Hence, \end{itemize}
-				body += re.sub(r'[}\]]', r'\\end{itemize}', line) + '\n'
-			else:
-				body += '\item $' + line + '$\n' #This is the typical case, where an item is surrounded by the LaTeX syntax for making a bullet point.
-	
-		body = re.sub(r'"(.*?)"', r'{\1}', body).replace('#', '\#').replace(',', '') #This is another regex command. LaTeX uses {} to denote strings, while JSON and Python use "". This regex replaces "" with {} by finding all characters (hence, the .*), making sure it's non greedy (hence, the ?), that fit in between two double quotes (hence, the double quotes around the (.*?)). The parentheses allow me to reference the string in between the quotes, which allows me to preserve it and reference it with a '\1' and then surround that \1 with { and }. We also want to get rid of the commas and escape the pound sign.
+        body = re.sub(r'"(.*?)"', r'{\1}', body).replace('#', '\#').replace(',', '') #This is another regex command. LaTeX uses {} to denote strings, while JSON and Python use "". This regex replaces "" with {} by finding all characters (hence, the .*), making sure it's non greedy (hence, the ?), that fit in between two double quotes (hence, the double quotes around the (.*?)). The parentheses allow me to reference the string in between the quotes, which allows me to preserve it and reference it with a '\1' and then surround that \1 with { and }. We also want to get rid of the commas and escape the pound sign.
 
-		output_filepath = filepath.replace('.py', '.tex')
-		output_filepath = filepath.replace('.json', '.tex') #I want the LaTeX file to have the same name as the original file passed in, which is why only the extension is replaced.
-		latex_file = open(output_filepath, 'w') #Opens the filepath with .tex ending, which creates a new .tex file.
-		latex_file.write(header + body + end) #Concatenates and writes the LaTeX code into the file.
-		latex_file.close() #Close the file. We're done!
+        output_filepath = filepath.replace('.py', '.tex')
+        output_filepath = filepath.replace('.json', '.tex') #I want the LaTeX file to have the same name as the original file passed in, which is why only the extension is replaced.
+        latex_file = open(output_filepath, 'w') #Opens the filepath with .tex ending, which creates a new .tex file.
+        latex_file.write(header + body + end) #Concatenates and writes the LaTeX code into the file.
+        latex_file.close() #Close the file. We're done!
--- local/lib/JTeX/smart_parser.py
+++ local/lib/JTeX/smart_parser.py
@@ -1,73 +1,90 @@
 #WARNING: UNFINISHED! DO NOT USE!
 import json
+import re
 
 class JsonTerminus(object):
-	def __init__(self, value):
-		self.value = value
+    def __init__(self, value):
+        self.value = value
 
-	def get_value(self):
-		return self.value
+    def get_value(self):
+        return self.value
 
-	def __str__(self):
-		return "(Terminus {0})".format(self.value)
+    def __str__(self):
+        return "(Terminus {0})".format(self.value)
 
 class JsonKey(object):
-	def __init__(self, key):
-		self.key = key
+    def __init__(self, key):
+        self.key = key
 
-	def get_value(self):
-		return self.key
+    def get_value(self):
+        return self.key
 
-	def __str__(self):
-		return "(JsonKey {0})".format(self.key)
+    def __str__(self):
+        return "(JsonKey {0})".format(self.key)
 
 class JsonObject(object):
-	def __init__(self):
-		self.attributes = {}
+    def __init__(self):
+        self.attributes = {}
 
-	def add(self, key, value):
-		self.attributes[key] = value
+    def add(self, key, value):
+        self.attributes[key] = value
 
-	def __str__(self):
-		result = "(JsonObject\n"
-		for attr in self.attributes:
-			result += "\t{0} {1}\n".format(str(attr), str(self.attributes[attr]))
-		result += ")\n"
-		return result
+    def __str__(self):
+        type_str = self.attributes[JsonKey("type")].get_value().split(".")[-1]
+        ps_type = lookup(ps_obj_str)
+            
+        x = self.attributes[JsonKey("x")].get_value()
+        y = self.attributes[JsonKey("y")].get_value()
+            
+        latex_str = "\\begin{{0}}\n STUFF \\end{{0}}\n".format(ps_type)
+
+    def __str_(self):
+        result = "(JsonObject\n"
+        for attr in self.attributes:
+            result += "\t{0} {1}\n".format(str(attr), str(self.attributes[attr]))
+        result += ")\n"
+        return result
 
 class JsonList(object):
-	def __init__(self):
-		self.json_objects = []
+    def __init__(self):
+        self.json_objects = []
 
-	def add(self, json_obj):
-		self.json_objects.append(json_obj)
-	
-	def __str__(self):
-		result = "(JsonList\n"
-		for obj in self.json_objects:
-			result += "{0}\n".format(str(obj))
-		result += ")\n"
-		return result
+    def add(self, json_obj):
+        self.json_objects.append(json_obj)
+    
+    def __str__(self):
+        result = "(JsonList\n"
+        for obj in self.json_objects:
+                result += "{0}\n".format(str(obj))
+        result += ")\n"
+        return result
 
-def parse(json_token):
-	#Todo
+def parse(json_obj):
+    return str(json_obj)
 
 def tokenize(py_obj):
-	if type(py_obj) == list:
-		json_list = JsonList()
-		for elem in py_obj:
-			json_list.add(tokenize(elem))
-		return json_list
-	if type(py_obj) == dict:
-		json_object = JsonObject()
-		for key in py_obj:
-			json_object.add(JsonKey(key), tokenize(py_obj[key]))
-		return json_object
-	else:
-		return JsonTerminus(py_obj)
+    if type(py_obj) == list:
+        json_list = JsonList()
+        for elem in py_obj:
+            json_list.add(tokenize(elem))
+        return json_list
+    if type(py_obj) == dict:
+        json_object = JsonObject()
+        for key in py_obj:
+            json_object.add(JsonKey(key), tokenize(py_obj[key]))
+        return json_object
+    else:
+        return JsonTerminus(py_obj)
+
+def preprocess(json_text):
+    return json.loads(json_text)
 
 def scan(filepath):
-	json_file = open(filepath, 'r')
-	json_text = json_file.read()
-	py_obj = json.loads(json_text)
-	return tokenize(py_obj)
+    json_file = open(filepath, 'r')
+    return json_file.read()
+
+def main(filepath):
+    json_text = scan(filepath)
+    py_obj = preprocess(json_text)
+    json_obj = tokenize(py_obj)
+    pstricks = parse(json_obj)
